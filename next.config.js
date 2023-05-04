316e45c71640d2806a5c3e40eb8fe8c54ec230ff
Negar <nasirinegar13@gmail.com>
2023-05-04 12:33:14 +0100

    adjusted next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
