c98fe177f4fa17f7e02fa99c1a708ea0e191993a
Negar <nasirinegar13@gmail.com>
2023-05-04 11:07:39 +0100

    added sidebarMenu data

---
+++ data/sidebarMenu.js
@@ -0,0 +1,34 @@
+export const sidebarMenu = [
+  {
+    title: "Dashboard",
+    href: "#",
+  },
+  {
+    title: "Music",
+    href: "#",
+  },
+  {
+    title: "Drink",
+    href: "/drinks",
+  },
+  {
+    title: "Shopping",
+    href: "#",
+  },
+  {
+    title: "Chat",
+    href: "#",
+  },
+  {
+    title: "Profile",
+    href: "#",
+  },
+  {
+    title: "Notification",
+    href: "#",
+  },
+  {
+    title: "Logout",
+    href: "#",
+  },
+];
