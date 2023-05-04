16dbd5d6775f366e2df6ffc43b5cfa9875088ed1
Negar <nasirinegar13@gmail.com>
2023-05-04 11:38:32 +0100

    changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,34 @@
-import './globals.css'
+"use client";
+import "./globals.css";
+import { Roboto, Poppins } from "@next/font/google";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { useMediaQuery } from "react-responsive";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import Footer from "./components/Footer";
+config.autoAddCss = false;
+
+const roboto = Roboto({
+  weight: ["400", "700", "100"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+const poppins = Poppins({
+  weight: ["400", "700", "100"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  // const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 });
+  // const isBigScreen = useMediaQuery({ minWidth: 1824 });
+  // const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
+
   return (
     <html lang="en">
       {/*
@@ -12,7 +36,17 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+
+      <body className={`${roboto.className} ${poppins.className}  bg-dark`}>
+        <div className=" w-screen flex ">
+          <Sidebar />
+          <div className="basis-5/6">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
