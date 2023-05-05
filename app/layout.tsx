12835de93d4a22cb37c57d8ffd437c6ef88b546d
Negar <nasirinegar13@gmail.com>
2023-05-05 20:38:05 +0100

    removed comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Roboto, Poppins } from "@next/font/google";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import { useMediaQuery } from "react-responsive";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Footer from "./components/Footer";
@@ -25,20 +24,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 });
-  // const isBigScreen = useMediaQuery({ minWidth: 1824 });
-  // const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
-
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-
       <body className={`${roboto.className} ${poppins.className}  bg-dark`}>
-        <div className=" w-screen flex ">
+        <div className=" w-screen flex flex-col lg:flex-row ">
           <Sidebar />
           <div className="basis-5/6">
             <Navbar />
