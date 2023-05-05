0d7241f66ee543105033a05043be3d5f28fda7ee
Negar <nasirinegar13@gmail.com>
2023-05-05 20:31:11 +0100

    created seperate component for searchBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,15 @@
 "client side";
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import MovieMenu from "./MovieMenu";
 import SearchBar from "./SearchBar";
 import UserMenu from "./UserMenu";
 function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="h-20 flex items-center px-10  ">
+    <div className="lg:h-20 flex items-center lg:px-10  h-[60px] relative  max-[640px]:w-[100%] max-[640px]:absolute top-0 left-0 z-10   ">
       <SearchBar />
       <div className="text-light mr-auto flex gap-20 h-full ">
-        <div className="group hover:bg-slate-700/60 relative ">
+        <div className="group hover:bg-slate-700/60 relative hidden lg:block z-20 ">
           <div className="flex items-center justify-center gap-3 w-full h-full px-4 ">
             <FontAwesomeIcon
               icon={faChevronDown}
@@ -21,14 +19,14 @@ function Navbar() {
           </div>
           <MovieMenu />
         </div>
-        <div className="flex items-center justify-center gap-3">
+        <div className="lg:flex items-center justify-center gap-3 hidden ">
           <FontAwesomeIcon
             icon={faChevronDown}
             style={{ fontSize: 12, color: "#807979", cursor: "pointer" }}
           />
           <span className="cursor-pointer">Serial</span>
         </div>
-        <div className="flex items-center justify-center gap-3">
+        <div className="lg:flex items-center justify-center gap-3 hidden ">
           <FontAwesomeIcon
             icon={faChevronDown}
             style={{ fontSize: 12, color: "#807979", cursor: "pointer" }}
@@ -36,18 +34,7 @@ function Navbar() {
           <span className="cursor-pointer">Artist</span>
         </div>
       </div>
-      <div
-        className="avatar flex items-center cursor-pointer relative"
-        onClick={() => setIsMenuOpen((pre) => !pre)}
-      >
-        <img
-          className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 mr-3"
-          src="/avatar.png"
-          alt="Bordered avatar"
-        />
-        <span className="text-light">John Doe</span>
-        <UserMenu isMenuOpen={isMenuOpen} />
-      </div>
+      <UserMenu />
     </div>
   );
 }
