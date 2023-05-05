fcd1e234a9e9b964bb4fc86c2888993f160a5410
Negar <nasirinegar13@gmail.com>
2023-05-05 20:28:47 +0100

    added eventListener for outside click

--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -1,45 +1,77 @@
 "client side";
-type UserMenuProps = {
-  isMenuOpen: boolean;
-};
+import { useRef, useEffect, useState } from "react";
 
-function UserMenu({ isMenuOpen }: UserMenuProps) {
-  console.log(isMenuOpen);
+function UserMenu() {
+  const userMenuRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        userMenuRef.current &&
+        !userMenuRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <div
-      className={`${
-        isMenuOpen === true ? "block" : "hidden"
-      } z-10 absolute bg-black rounded-lg shadow w-44 top-[59px] right-[0px] text-white`}
+      className="avatar flex items-center relative  h-full px-2"
+      ref={userMenuRef}
     >
-      <ul
-        className="py-2 text-sm text-white"
-        aria-labelledby="dropdownDefaultButton"
+      <div
+        className="flex items-center cursor-pointer"
+        onClick={() => setIsMenuOpen((pre) => !pre)}
       >
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:font-semibold hover:text-red-700"
-          >
-            profile
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2 hover:font-semibold hover:text-red-700"
-          >
-            Active Service
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block px-4 py-2  hover:font-semibold hover:text-red-700"
-          >
-            Log Out
-          </a>
-        </li>
-      </ul>
+        <img
+          className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 mr-3"
+          src="/avatar.png"
+          alt="Bordered avatar"
+        />
+        <span className="text-light">John Doe</span>
+      </div>
+
+      <div
+        className={`${
+          isMenuOpen === true ? "block" : "hidden"
+        } z-10 absolute bg-black rounded-lg shadow w-44 top-[100%] right-[0px] text-white`}
+      >
+        <ul
+          className="py-2 text-sm text-white"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:font-semibold hover:text-red-700"
+            >
+              profile
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:font-semibold hover:text-red-700"
+            >
+              Active Service
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2  hover:font-semibold hover:text-red-700"
+            >
+              Log Out
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
