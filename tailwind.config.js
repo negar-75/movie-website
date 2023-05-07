ba031f07aaf34497791a570125c104bf97930659
Negar <nasirinegar13@gmail.com>
2023-05-07 12:24:09 +0100

    added animation delay plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -7,6 +7,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    transitionDelay: {
+      100: "100ms",
+      200: "200ms",
+      300: "300ms",
+      400: "400ms",
+      500: "500ms",
+      600: "600ms",
+      700: "700ms",
+      800: "800ms",
+      900: "900ms",
+      1000: "1000ms",
+      1100: "1100ms",
+      1200: "1200ms",
+      1300: "1300ms",
+      1400: "1400ms",
+      1500: "1500ms",
+      1600: "1600ms",
+      1700: "1700ms",
+      1800: "1800ms",
+    },
     extend: {
       fontFamily: {
         roboto: ["var(--font-roboto)"],
@@ -33,5 +53,20 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-3d"), require("tailwindcss-animation-delay")],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "animation-delay": (value) => {
+            return {
+              " animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme("transitionDelay"),
+        }
+      );
+    }),
+  ],
 };
