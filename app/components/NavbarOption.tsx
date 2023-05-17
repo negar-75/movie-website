7ab7903b6c7e670e4c8004a644c076557db8cf11
Negar <nasirinegar13@gmail.com>
2023-05-17 09:25:09 +0100

    created NavbarOption component

---
+++ app/components/NavbarOption.tsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+type NavbarOptionProps = {
+  name: string;
+  menu?: React.ReactNode;
+};
+
+function NavbarOption({ name, menu }: NavbarOptionProps) {
+  return (
+    <div className="group hover:bg-slate-700/60 relative flex z-20 ">
+      <div className="flex items-center justify-center gap-3 w-full h-full px-4 ">
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          style={{ fontSize: 12, color: "#807979", cursor: "pointer" }}
+        />
+        <span className=" cursor-pointer group">{name}</span>
+      </div>
+      {menu}
+    </div>
+  );
+}
+
+export default NavbarOption;
