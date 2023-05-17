322a6a0210959aa46541475ad2e5d9877076758a
Negar <nasirinegar13@gmail.com>
2023-05-17 13:33:22 +0100

    added div wrapped the icon

--- app/components/MobileSideBarMenu.tsx
+++ app/components/MobileSideBarMenu.tsx
@@ -2,6 +2,7 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import Logo from "./Logo";
 
 type MobileSideBarMenuProps = {
   isMenuOpen: boolean;
@@ -15,13 +16,46 @@ function MobileSideBarMenu({
   return (
     <div
       className={`${
-        isMenuOpen ? "block" : "hidden"
-      } fixed top-0 right-0 left-0 h-screen bg-red-500 transition-all duration-75`}
+        isMenuOpen ? "grid" : "hidden"
+      }  grid-rows-12  gap-y-10 fixed top-0 right-0 left-0 h-screen bg-neutral-300 transition-all duration-75 md:hidden `}
     >
-      <FontAwesomeIcon
-        icon={faClose}
-        className="text-2xl"
-        onClick={() => setIsMenuOpen(false)}
+      <div className="py-[10px] justify-center row-[1/3] flex items-center">
+        <div
+          onClick={() => setIsMenuOpen(false)}
+          className=" border-black border-4 h-[40px] w-[40px] rounded-full flex items-center justify-center p-6 cursor-pointer hover:border-red-800"
+        >
+          <FontAwesomeIcon
+            icon={faClose}
+            className="text-3xl text-red-800 "
+          />
+        </div>
+      </div>
+
+      <div className=" row-[3/12]">
+        <ul className="text-center text-3xl space-y-5">
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Movies
+          </li>
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Serials
+          </li>
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Artist
+          </li>
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Shopping
+          </li>
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Drinks
+          </li>
+          <li className="cursor-pointer  py-2 hover:translate-x-3 transition-transform ease-in duration-200">
+            Profile
+          </li>
+        </ul>
+      </div>
+      <Logo
+        fontSize="text-xl"
+        height="h-20"
       />
     </div>
   );
