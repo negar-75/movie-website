27e142a4f073c04bc4a1faf9b4494370e33cd9d1
Negar <nasirinegar13@gmail.com>
2023-05-18 16:40:54 +0100

    added font file

---
+++ styles/font.ts
@@ -0,0 +1,14 @@
+import { Poppins, Playfair_Display } from "@next/font/google";
+
+const playfair_display = Playfair_Display({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+const poppins = Poppins({
+  weight: ["400", "700", "100"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export { playfair_display, poppins };
