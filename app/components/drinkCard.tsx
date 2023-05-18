81df8a1455447ce55372fc163e41fe4e68fcedbc
Negar <nasirinegar13@gmail.com>
2023-05-18 20:13:29 +0100

    changed height

--- app/components/drinkCard.tsx
+++ app/components/drinkCard.tsx
@@ -14,9 +14,9 @@ function DrinkCard({ imageUrl, desc, title }: DrinkProps) {
   return (
     <div
       style={{ "--image-url": `url(${imageUrl})` } as React.CSSProperties}
-      className={`md:flex md:flex-row  md:h-[550px] h-[600px] rounded-lg overflow-hidden relative max-[640px]:bg-[image:var(--image-url)] max-[640px]:bg-cover `}
+      className={`md:flex md:flex-row  md:h-[550px] h-[620px] rounded-lg overflow-hidden relative max-[640px]:bg-[image:var(--image-url)] max-[640px]:bg-cover `}
     >
-      <div className="md:basis-[40%] bg-fuchsia-500 basis-[70%]">
+      <div className="md:basis-[40%]  basis-[70%]">
         <div className="relative h-[100%]">
           <Image
             src={imageUrl}
@@ -26,7 +26,7 @@ function DrinkCard({ imageUrl, desc, title }: DrinkProps) {
           />
         </div>
       </div>
-      <div className="md:basis-[60%] bg-stone-300 md:p-10 flex flex-col   md:justify-between max-[640px]:h-[45%] max-[640px]:bg-lightRed p-5 max-[640px]:absolute bottom-0 ">
+      <div className="md:basis-[60%] bg-stone-300 md:p-10 flex flex-col   md:justify-between max-[640px]:h-[50%] max-[640px]:bg-lightRed p-5 max-[640px]:absolute bottom-0 ">
         <span
           className={`${playfair_display.className} max-[640px]:mb-5 text-center  border-y-2 md:py-2 md:px-5 border-black text-2xl font-semibold md:text-4xl`}
         >
