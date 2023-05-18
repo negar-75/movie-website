bffb36d369f5bb06bc0ed0da4b7677a515cc1ff7
Negar <nasirinegar13@gmail.com>
2023-05-18 16:47:49 +0100

    replaced upper case with lower case

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
-import SearchBar from "./SearchBar";
-import UserMenu from "./UserMenu";
-import NavbarOptions from "./NavbarOptions";
-import Logo from "./Logo";
-import MobileSidebar from "./MobileSidebar";
+import SearchBar from "./searchBar";
+import UserMenu from "./userMenu";
+import NavbarOptions from "./navbarOptions";
+import Logo from "./logo";
+import MobileSidebar from "./mobileSidebar";
 function Navbar() {
   return (
     <div className="md:h-20 flex items-center md:px-10  h-[60px] relative  max-[640px]:w-[100%]  top-0 left-0 z-10   ">
