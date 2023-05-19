b3e365334c2cbd360abbcb14a773e0a56f813155
Negar <nasirinegar13@gmail.com>
2023-05-19 08:58:22 +0100

    added pr

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -1,7 +1,7 @@
 function Title({ title }: { title: string }) {
   return (
     <div className="title text-white font-semibold mb-8 relative flex items-center pr-4 ">
-      <span className="flex-shrink lg:text-2xl text-white px-4 text-lg">
+      <span className="flex-shrink lg:text-2xl text-white pr-4 text-lg">
         {title}
       </span>
       <div className="flex-grow h-px bg-light"></div>
