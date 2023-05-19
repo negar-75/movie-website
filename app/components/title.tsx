19a8e1759ef25bd3c83f03ce2051f921de03cfef
Negar <nasirinegar13@gmail.com>
2023-05-19 23:10:07 +0100

    added home rendered conditionally

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -1,9 +1,16 @@
-function Title({ title }: { title: string }) {
+"use client";
+
+import { useState, useEffect } from "react";
+
+function Title({ title, home }: { title: string; home?: boolean }) {
+  const [showHome, setShowHome] = useState<boolean>(false);
+  useEffect(() => setShowHome(home as boolean), []);
   return (
     <div className="title text-white font-semibold mb-8 relative flex items-center pr-4 ">
-      <span className="flex-shrink lg:text-2xl text-white pr-4 text-lg">
+      <p className="flex-shrink lg:text-2xl text-white pr-4 text-lg">
+        {showHome && <a href="/">Home/</a>}
         {title}
-      </span>
+      </p>
       <div className="flex-grow h-px bg-light"></div>
     </div>
   );
