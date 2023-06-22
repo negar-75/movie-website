2671a60515f9cfcd5eb0101c40ad53cd0c3bee34
Negar <nasirinegar13@gmail.com>
2023-06-22 08:26:00 +0100

    removed console.log

--- app/api/top-rated/route.ts
+++ app/api/top-rated/route.ts
@@ -16,13 +16,11 @@ export async function GET() {
   cache = JSON.parse(cache as string);
   let result: Partial<Result> = {};
   if (cache) {
-    console.log("loading from cache");
     result.data = cache;
     result.type = "redis";
     result.latency = Date.now() - start;
     return NextResponse.json(result);
   } else {
-    console.log("loading from api");
     start = Date.now();
     return fetch(
       `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=1`
