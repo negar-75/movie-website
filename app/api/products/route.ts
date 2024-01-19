e4a46b6b3405474eeee02e34c79af9e3a8c5c4d3
Negar <nasirinegar13@gmail.com>
2024-01-19 14:03:21 +0000

    took out firebaseConfigue

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,19 +1,7 @@
 import { NextResponse } from "next/server";
-import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
+import  firebaseApp  from "../../../firebase/firebaseInit";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyADBB5Vl4y0myYxCEKPxBLw93fLeotR2pE",
-  authDomain: "cinema-city-e891f.firebaseapp.com",
-  databaseURL: "https://cinema-city-e891f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cinema-city-e891f",
-  storageBucket: "cinema-city-e891f.appspot.com",
-  messagingSenderId: "181995918794",
-  appId: "1:181995918794:web:62017505a0c048fd5a2b33",
-  measurementId: "G-E3T8ZXMTWH"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
 const productsRef = ref(database, "products"); 
 
