6ec2da585349b1cd07f0e42ef5902536ac1befaf
Negar <nasirinegar13@gmail.com>
2024-01-19 14:00:53 +0000

    refactored and removed useClient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
-"use client";
 import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Head from "next/head";
+import ProfileProvider from "../context/context";
 
 config.autoAddCss = false;
-
-// const roboto = Roboto({
-//   weight: ["400", "700", "100"],
-//   subsets: ["latin"],
-//   variable: "--font-roboto",
-// });
-// const poppins = Poppins({
-//   weight: ["400", "700", "100"],
-//   subsets: ["latin"],
-//   variable: "--font-poppins",
-// });
 
 export default function RootLayout({
   children,
@@ -25,16 +14,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="movie website"
-        />
+        <meta name="description" content="movie website" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no"
         ></meta>
       </Head>
-      <body className={`  bg-dark`}>{children}</body>
+      <body className={` bg-dark`}>
+        <ProfileProvider>{children}</ProfileProvider>
+      </body>
     </html>
   );
 }
