c36eb9d323772615ffb1ad83edfa2fdc621242b9
Negar <nasirinegar13@gmail.com>
2024-01-19 14:02:08 +0000

    created signup page

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+
+import WordBetweenLines from "@/app/components/wordBetweenLines";
+import { ChangeEvent, useState } from "react";
+import SocialMedias from "@/app/components/socialMedias";
+import {
+  handleInputChange,
+  handleSubmit,
+} from "@/helpers/formActions";
+import { registerInputsList } from "../../../data/registerInputsList";
+import RegistrationForm from "../../components/registrationForm";
+import { registerFormValidationScheme } from "../../../types/forms";
+import { UserRegister } from "@/types/user";
+import { useProfile } from "../../../context/context";
+import { UserContextApi } from "@/types/user";
+
+function SignUpPage() {
+  const [user, setUser] = useState<UserRegister>({
+    username: "",
+    password: "",
+    repeatPassword: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState<Partial<UserRegister>>({});
+  const context: Partial<UserContextApi<UserRegister>> = useProfile();
+  console.log(user);
+  console.log(context)
+  return (
+    <div className="w-full flex flex-col items-center">
+    <RegistrationForm
+          onSubmit={(event: React.FormEvent) => {
+            if (context.register) {
+              handleSubmit(
+                event,
+                registerFormValidationScheme,
+                user,
+                context.register,
+                setErrors
+              );
+            }
+          }}
+          onInputChange={(event: ChangeEvent<HTMLInputElement>) => {
+            handleInputChange(event, setUser);
+          }}
+          errors={errors}
+          inputsList={registerInputsList}
+          buttonText="submit"
+        />
+        <WordBetweenLines word="OR" />
+        <SocialMedias />
+      </div>
+        
+    
+  );
+}
+
+export default SignUpPage;
